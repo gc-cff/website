7c222e4ee2a93434370b2bed311dab564c49e75b
scar17off <scar17off@gmail.com>
2025-08-29 03:10:18 +0300

    Changed Router from BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './index.css';
 import './css/Button.css';
@@ -12,7 +12,7 @@ import DocsView from './views/DocsView';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename="/website">
+    <Router>
       <Navbar />
       <main>
         <Routes>
